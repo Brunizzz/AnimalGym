a17004b12f0d6ac89925e82a96a3cdad39f0eeaf
Miguel150404 <m.angel.rdzl15@gmail.com>
2025-06-16 10:20:53 -0600

    Creacion componentes para agregar usuarios y productos

---
+++ ProyectoFinal_front/src/app/components/agregar-productos/agregar-productos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-productos',
+  imports: [],
+  templateUrl: './agregar-productos.component.html',
+  styleUrl: './agregar-productos.component.css'
+})
+export class AgregarProductosComponent {
+
+}
---
+++ ProyectoFinal_front/src/app/components/agregar-usuarios/agregar-usuarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-usuarios',
+  imports: [],
+  templateUrl: './agregar-usuarios.component.html',
+  styleUrl: './agregar-usuarios.component.css'
+})
+export class AgregarUsuariosComponent {
+
+}
